0a50ab4eb163c30f888f8e0fbe6c5bb0fd1e23dc
Amit Gunjal <amitgunjal26@gmail.com>
2017-04-18 02:27:44 +0530

    vector space model partially completed.

--- src/mini_qa.py
+++ src/mini_qa.py
@@ -3,31 +3,42 @@ import wikipedia
 import nltk
 from nltk.corpus import stopwords
 from textblob import TextBlob
+import time
+from nltk.stem.snowball import SnowballStemmer
+import re
 import train_ans_type
+import vsm
 import download_data
-
-# Q. when tom cruise was born?
+# Q. when thomas cruise was born?
 # Q. Which country has hindi as an official language other than india?
-stop_words_list = set(stopwords.words('english'))
 
 
-def ask():
-    question = input("Q. ").strip()
-    question = question.lower()
-    ans_typ = train_ans_type.ans_type(question)
-    print('Answer type : ', ans_typ)
-    q_tokens = nltk.word_tokenize(question)
-    print(q_tokens)
-    question = question.replace("?", "")
-    good_q = [i for i in question.lower().split() if i not in stop_words_list]
-    print(good_q)
-    search_query = " ".join(good_q)
-    # print(search_query)
-    download_data.fetch(question)
+class MiniQA:
+    stemmer = SnowballStemmer("english")  # Applied Stemming
+    stop_words_list = set(stopwords.words('english'))
+
+    def ask(self):
+        question = input("Q. ").strip()
+        question = question.lower()
+        ans_typ = train_ans_type.ans_type(question)
+        print('Answer type : ', ans_typ)
+        q_tokens = nltk.word_tokenize(question)
+        # print(q_tokens)
+        question = question.replace("?", "")
+        good_q = [i for i in question.lower().split() if i not in self.stop_words_list]
+        download_data.fetch(good_q)
+        # print(good_q)
+        vsm.search(question)
 
 
-ask()
+start_time = time.time()
+m = MiniQA()
+m.ask()
+end_time = time.time()
+print('')
+print('Total ', (end_time-start_time), " seconds.")
 '''
+
 
 # q_tagged = nltk.pos_tag(q_tokens)
 # print(q_tagged)
@@ -38,7 +49,7 @@ ask()
 # mean = TextBlob(q).sentiment
 # print(mean)
 q = q.replace("?", "")
-good_q = [i for i in q.lower().split() if i not in stop_words_list]
+good_q = [i for i in q.lower().split() if i not in self.stop_words_list]
 print(good_q)
 
 ans = wikipedia.search()
