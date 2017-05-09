d6a8959223c6a82260dcf61864d9e8e339cc62a7
Amit Gunjal <amitgunjal26@gmail.com>
2017-05-09 16:58:05 +0530

    Answerd a typical question.

--- src/answer_engine.py
+++ src/answer_engine.py
@@ -3,17 +3,24 @@ import nltk.data
 from corenlp import *
 import collections
 import difflib
+from nltk.corpus import stopwords
 
 commonwords = ["the", "a", "an", "is", "are", "were", "."]
-target = ['important', 'aspect', 'CRM', 'approach']
 
+stop = set(stopwords.words('english'))
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-
 path = os.path.dirname(os.path.realpath(__file__))
 path = path.replace("/src",  "/corpora/")
 
-article = open(path+'Customer_relationship_management.txt', 'r')
+q = input("Q. ")
+q = q.replace('?', '')
+q_tag = nltk.word_tokenize(q)
+target = [i for i in q.lower().split() if i not in stop]
+print(target)
+
+article = open(path+'Customer_experience.txt', 'r')
 file_data = article.read()
+file_data.lower()
 sentence = tokenizer.tokenize(file_data)
 # print(sentence)
 
@@ -23,4 +30,11 @@ for i in range(0, len(sentence)):
     # print(sentence[i])
     seq = difflib.SequenceMatcher(None, searchstring, sentence[i])
     d = seq.ratio() * 100
-    print(d, '--->', sentence[i])
+    # print(d, '--->', sentence[i])
+
+conf = 0
+for i in range(0, len(sentence)):
+    sent_tok = nltk.word_tokenize(sentence[i])
+    # print(sent_tok)
+    if sent_tok.__contains__(target[0] or target[1] or target[2] or target[3] or target[4] or target[5]):
+        print(sentence[i])
